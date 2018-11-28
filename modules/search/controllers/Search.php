55082b22d31b6b2c045768844ef5ec4b676edf5c
edwin-enfin <edwin@enfintechnologies.com>
2018-11-28 09:27:27 +0530

    test

--- modules/search/controllers/Search.php
+++ modules/search/controllers/Search.php
@@ -8,7 +8,7 @@ class Search extends CI_Controller
 	{
 		parent::__construct();
 		
-		/* CACHE CONTROL*/
+		/* CACHE CONTROL test*/
 		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
 		$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 		$this->output->set_header('Pragma: no-cache');
