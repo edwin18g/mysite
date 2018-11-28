d7d914fcc75b6a908f553b4912ed436b5bbff35b
edwin-enfin <edwin@enfintechnologies.com>
2018-11-29 00:15:54 +0530

    td

--- modules/user/views/home.php
+++ modules/user/views/home.php
@@ -8,87 +8,105 @@
             <div class="" style="position:relative">
                 <div class="btn-group btn-group-justified">
                     <a class="ajaxloads btn btn-default btn-lg active" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar" style="border-right:none"><i class="fa fa-newspaper-o"></i><span class="hidden-xs"> &nbsp; Administration</span></a>
-                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/friends" style="border-right:none"><i class="fa fa-users"></i><span class="hidden-xs"> &nbsp; Priests</span> <small class="badge" id="friends-count">0</small></a>
-                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/followers" style="border-right:none"><i class="fa fa-retweet"></i><span class="hidden-xs"> &nbsp; Parishes</span> <small class="badge" id="followers-count">0</small></a>
-                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/following"><i class="fa fa-random"></i><span class="hidden-xs"> &nbsp; Organizations</span> <small class="badge" id="following-count">0</small></a>
-                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/followers" style="border-right:none"><i class="fa fa-retweet"></i><span class="hidden-xs"> &nbsp; History</span> <small class="badge" id="followers-count">0</small></a>
-                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/following"><i class="fa fa-random"></i><span class="hidden-xs"> &nbsp; Contact Us</span> <small class="badge" id="following-count">0</small></a>
+                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/friends" style="border-right:none"><i class="fa fa-users"></i><span class="hidden-xs"> &nbsp; Priests</span> </a>
+                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/followers" style="border-right:none"><i class="fa fa-retweet"></i><span class="hidden-xs"> &nbsp; Parishes</span> </a>
+                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/following"><i class="fa fa-random"></i><span class="hidden-xs"> &nbsp; Organizations</span> </a>
+                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/followers" style="border-right:none"><i class="fa fa-retweet"></i><span class="hidden-xs"> &nbsp; History</span> </a>
+                    <a class="ajaxloads btn btn-default btn-lg" href="http://kuzhithuraidiocese.com/new/fr-jaya-kumar/following"><i class="fa fa-random"></i><span class="hidden-xs"> &nbsp; Contact Us</span> </a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<div id="profileView" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal_table">
-        <div class="modal-content modal_cell">
-            <div class="modal-content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
-                                <h3 id="myModalLabel"><i class="fa fa-user"></i> &nbsp; kuzhi</h3>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-horizontal">
-                                    <div class="form-group">
-                                        <div class="col-xs-10 col-xs-offset-1">
-                                            <h4><i class="fa fa-info-circle"></i> <?php echo phrase('account_details'); ?></h4>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-1 col-xs-offset-1 text-left"><i class="fa fa-quote-left"></i></label>
-                                        <div class="col-xs-9">
-                                            text
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-1 col-xs-offset-1 text-left"><i class="fa fa-map-marker"></i></label>
-                                        <div class="col-xs-9">
-                                            text
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-1 col-xs-offset-1 text-left"><i class="fa fa-mobile"></i></label>
-                                        <div class="col-xs-9">
-                                            text
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-1 col-xs-offset-1 text-left"><i class="fa fa-venus-mars"></i></label>
-                                        <div class="col-xs-9">
-                                            text
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-1 col-xs-offset-1 text-left"><i class="fa fa-child"></i></label>
-                                        <div class="col-xs-9">
-                                          text
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <div class="form-group">
-                                    <div class="col-xs-4 text-left">
-                                        <button type="button" data-dismiss="modal" class="btn btn-default"><i class="fa fa-times"></i> <?php echo phrase('close'); ?></button>
-                                    </div>
-                                    <div class="col-xs-8">
-                                        
-                                            
-                                                <span class="btn-group">
-                                                   
-                                                </span>
-                                            
-                                    
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="row">
+					<div class="col-md-4">
+						<div class="whitebg mleft">
 
+
+							<h2>&nbsp;Welcome</h2>
+							
+							<p>
+								Pope Francis created a new Latin rite Diocese of Kuzhithurai, carved out of the Diocese of Kottar, in Kanniyakumari District, Tamil Nadu, India making it a suffragan of the metropolitan see of Madurai and with great joy and pastoral concern, he announced it officially on 22nd December 2014 at 12.00 hrs, Italian Time/16.30 hrs (IST) appointing Fr. Jeromedhas Varuvel S.D.B. as its first Bishop. 
+							</p>
+							<p class="textwrapl readbuttonright"><a href="welcome.php" role="button" class="btn">Read More...</a></p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="whitebg">
+							<div class="tabbable" id="btabs">
+								<ul class="nav nav-tabs">
+									<li class="active">
+										<a href="#panelbishop" data-toggle="tab">Bishop</a>
+									</li>
+									<li>
+										<a href="#panelbcircular" data-toggle="tab">Circulars</a>
+									</li>
+									<li>
+										<a href="#panelnewsletter" data-toggle="tab">Newsletter</a>
+									</li>
+									<li>
+										<a href="#panelbcal" data-toggle="tab">Calendar</a>
+									</li>
+								</ul>
+								<div class="tab-content">
+									<div class="tab-pane active" id="panelbishop">
+
+										<img src="images/bishop-message.png" class="textwrap">
+										<p>The Most Rev. Msgr. V. Jeromedhas, SDB was born on October 21, 1951 at Paduvoor, Diocese of Kottar. <a href="aboutbishop.php">Read More ...</a></p>
+										
+										<p class="textwrap readbuttonup"><a href="#bishopmessageenglish" role="button" class="btn" data-toggle="modal">Message</a></p>
+										<p class="textwrap readbuttondown"><a href="#bishopmessagetamil" role="button" class="btn" data-toggle="modal"><strong>தமிழில்</strong></a></p>
+										<p></p>
+										
+									</div>
+									<div class="tab-pane" id="panelbcircular">
+									
+							<p>
+								<a href="circular/circular.pdf" target="“_blank”">Circular</a>
+							</p>
+					
+
+
+									</div>
+									<div class="tab-pane" id="panelnewsletter">
+									
+							<p>
+								<a href="newsletter/January-16.pdf" target="“_blank&quot;">Newsletter January 2016</a>
+							</p>
+							<p>
+								<a href="newsletter/December-15.pdf" target="“_blank”">Newsletter December 2015</a>
+							</p>
+							<p>
+								<a href="newsletter/November-15.pdf" target="“_blank”">Newsletter November 2015</a>
+							</p>
+<a href="newsletter/News letter - August 2018.pdf" target="“_blank”"> News Letter - August 2018<p></p>
+						
+
+
+									</a></div><a href="newsletter/News letter - August 2018.pdf" target="“_blank”">
+
+									</a><div class="tab-pane" id="panelbcal"><a href="newsletter/News letter - August 2018.pdf" target="“_blank”">
+										<iframe src="https://www.google.com/calendar/embed?showPrint=0&amp;height=500&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=rcdiocesekuzhithurai%40gmail.com&amp;color=%231B887A&amp;ctz=Asia%2FCalcutta" width="100%" height="200" scrolling="yes" class="iframe-class" frameborder="0"></iframe><br>
+										</a><center><a href="newsletter/News letter - August 2018.pdf" target="“_blank”"></a><a href="calendar.php">Click here to Enlarge Calendar</a></center>
+
+									</div>
+								</div>
+							</div>
+							
+
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="whitebg mright">
+							<h2>&nbsp;News &amp; Events</h2>
+<p="up" scrollamount="6" onmouseover="this.setAttribute('scrollamount', 0, 0);" onmouseout="this.setAttribute('scrollamount', 6, 0);">
+							
+							<hr>
+
+							<p>
+		The release of Common Accounting Software System to keep the accounts of parishes of Kuzhithurai Diocese on 01/08/2018 at St. Xavier's Catholic College of Engineering, Chunkankadai. We thank the software cell of the IT department of St Xavier's Catholic college of Engineering for their innovating and creative work.
+							</p>
+<p></p>
+						</p="up"></div>
+					</div>
+				</div>
