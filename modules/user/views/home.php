c92708a6233d4b293fcb08a4e8e44d992774a994
edwin-enfin <edwin@enfintechnologies.com>
2018-12-03 08:51:25 +0530

    test

--- modules/user/views/home.php
+++ modules/user/views/home.php
@@ -69,6 +69,7 @@
 					
 					<div class="col-md-3">
 						<div class="whitebg mright">
+						<?php echo widget_newOpenletters(1); ?>
 							<h2>&nbsp;News &amp; Events</h2>
 <p="up" scrollamount="6" onmouseover="this.setAttribute('scrollamount', 0, 0);" onmouseout="this.setAttribute('scrollamount', 6, 0);">
 							
